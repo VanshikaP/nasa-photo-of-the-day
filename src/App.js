8338f845a7e4b7181ed3b0459b6c3ad004b72fd2
VanshikaP <vanshikapundir92@gmail.com>
2020-02-20 13:12:29 -0800

    Added Styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
-import Card from './components/Card'
+import APOD from './components/APOD';
+import styled from 'styled-components';
+
+const AppContainer = styled.div `
+  background-image: url('https://www.nasa.gov/sites/default/files/thumbnails/image/darkrift-orig_full.jpg');
+  color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-      <Card />
-    </div>
+    <AppContainer className="App">
+      <h1>
+        NASA - Picture of the Day 
+      </h1>
+      <APOD />
+    </AppContainer>
   );
 }
 
