cd45ee679e4e79cc09a889d59df20e67b93c6159
VanshikaP <vanshikapundir92@gmail.com>
2020-02-20 13:29:36 -0800

    Finalised styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./App.css";
 import APOD from './components/APOD';
-import styled from 'styled-components';
 
-const AppContainer = styled.div `
-  background-image: url('https://www.nasa.gov/sites/default/files/thumbnails/image/darkrift-orig_full.jpg');
-  color: white;
-`
 
 function App() {
   return (
-    <AppContainer className="App">
+    <div className="App">
       <h1>
         NASA - Picture of the Day 
       </h1>
       <APOD />
-    </AppContainer>
+    </div>
   );
 }
 
